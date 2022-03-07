bb2071cb73b255af4fd21fc7b908c47c8dcf4cf6
Esfandyar Poozesh <epoozesh@gmail.com>
2022-03-07 12:55:17 -0700

    added pdfs folder and added author to readme

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Introduction
 
-This application was created in order to make accessing recipes easy.  Ideally, it will run on a device in the kitchen that the user can use to access recpies very quickly.  Recpies are stored in PDF format in /public/pdfs directory.
+This application was created in order to make accessing recipes easy. Ideally, it will run on a device in the kitchen that the user can use to access recpies very quickly. Recpies are stored in PDF format in /public/pdfs directory.
 
-# Installation 
+# Installation
 
 1.  Run `npm install` to install all modules.
 2.  Run application using `node app.js` or `nodemon app.js`.
@@ -11,12 +11,14 @@ Access application on [localhost:3000](http://localhost:3000)
 
 # Use
 
-Currently, the upload feature accepts all file types.  Eventually, only PDF format will be accepted.  When uploading, file will be uploaded to the `/public/pdfs` directory. 
+Currently, the upload feature accepts all file types. Eventually, only PDF format will be accepted. When uploading, file will be uploaded to the `/public/pdfs` directory.
 
 # Coming Soon
 
 - Integrate search capability to filter through list of recipes.
-- When uploading, user can upload any file type.  Will change to only allow PDF.
+- When uploading, user can upload any file type. Will change to only allow PDF.
 - Clean up upload success/fail dialog.
 - Decide wether to continue using boostrap for styling or bake one from scratch.
 - File deletion
+
+Created by Esfandyar Poozesh
--- app.js
+++ app.js
@@ -1,83 +1,85 @@
-const fs = require('fs')
-const path = require('path')
-const hbs = require('hbs')
-const express = require('express')
-const multer = require('multer')
+const fs = require("fs");
+const path = require("path");
+const hbs = require("hbs");
+const express = require("express");
+const multer = require("multer");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 let storage = multer.diskStorage({
-    destination: function(req, file, callback) {
-        callback(null, './public/pdfs');
-    },
-    filename: function(req, file, callback) {
-        callback(null, file.originalname);
-    }
+  destination: function (req, file, callback) {
+    callback(null, "./public/pdfs");
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.originalname);
+  },
 });
 
-let upload = multer({ storage }).single('pdf');
+let upload = multer({ storage }).single("pdf");
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, './public')
-const viewsPath = path.join(__dirname, './templates/views')
-const partialsPath = path.join(__dirname, './templates/partials')
+const publicDirectoryPath = path.join(__dirname, "./public");
+const viewsPath = path.join(__dirname, "./templates/views");
+const partialsPath = path.join(__dirname, "./templates/partials");
 
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
-app.get('/', (req, res) => {
-    const files = fs.readdirSync(path.join(__dirname, 'public/pdfs')).map(name => {
-        return {
-            name: path.basename(name, 'pdf'),
-            url: `/pdfs/${name}`
-        };
-    })
-
-    res.render('index', {
-        files,
-        title: 'Poozman Recipes',
-        name: 'Esfandyar Pooezsh',
-        listLength: files.length
-    })
-})
-
-app.get('/help', (req, res) => {
-    res.render('help', {
-        title: "Help",
-        name: "Esfandyar Poozesh"
-    })
-})
-
-app.get('/upload', (req, res) => {
-    let message = 'Select file to upload'
-    res.render('upload', {
-        title: 'Upload New Recipe',
-        name: 'Esfandyar Poozesh',
-    })
-})
-
-app.post('/up/pdf', function(req, res) {
-    upload(req, res, function(err) {
-        if (err) {
-            res.render('upload', {
-                title: 'Upload New Recipe',
-                name: 'Esfandyar Poozesh',
-                messageFail: 'Upload failed. Please try again.'
-            })
-        }
-            res.render('upload', {
-                title: 'Upload New Recipe',
-                name: 'Esfandyar Poozesh',
-                messageSuccess: 'Upload Successful.'
-        })
+app.get("/", (req, res) => {
+  const files = fs
+    .readdirSync(path.join(__dirname, "public/pdfs"))
+    .map((name) => {
+      return {
+        name: path.basename(name, "pdf"),
+        url: `/pdfs/${name}`,
+      };
     });
+
+  res.render("index", {
+    files,
+    title: "PDF Archives",
+    name: "Esfandyar Pooezsh",
+    listLength: files.length,
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    name: "Esfandyar Poozesh",
+  });
+});
+
+app.get("/upload", (req, res) => {
+  let message = "Select file to upload";
+  res.render("upload", {
+    title: "Upload New Recipe",
+    name: "Esfandyar Poozesh",
+  });
+});
+
+app.post("/up/pdf", function (req, res) {
+  upload(req, res, function (err) {
+    if (err) {
+      res.render("upload", {
+        title: "Upload New Recipe",
+        name: "Esfandyar Poozesh",
+        messageFail: "Upload failed. Please try again.",
+      });
+    }
+    res.render("upload", {
+      title: "Upload New Recipe",
+      name: "Esfandyar Poozesh",
+      messageSuccess: "Upload Successful.",
+    });
+  });
 });
 
 app.listen(port, () => {
-    console.log(`The server started correct on port ${port}...`)
-})
+  console.log(`The server started correct on port ${port}...`);
+});
